17dcde94c0719cc4d8516d58eeeff0b918181ec5
rpitta <raphael.pitta@hotmail.com>
2020-09-14 20:24:22 -0300

    Teste

--- src/main/resources/static/angular/public/js/services/loginService.js
+++ src/main/resources/static/angular/public/js/services/loginService.js
@@ -1,10 +1,10 @@
 angular.module('myHeroTraining').factory('loginService',function ($http){
     var autenticar = function (dadosUsuario) {
 
-       return $http.post('http://localhost:8080/auth',dadosUsuario)
+       return $http.post('/auth',dadosUsuario)
     }
     var dadosCadastro = function (email) {
-        return $http.get('http://localhost:8080/cadastro-usuario', {
+        return $http.get('/cadastro-usuario', {
             params: {
                 email: email
             }
