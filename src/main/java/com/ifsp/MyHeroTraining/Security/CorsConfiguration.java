7e06979e838e0da0f7714fad81144bc86f198239
rpitta <raphael.pitta@hotmail.com>
2020-09-14 20:35:36 -0300

    Mais alteracoes novas2

--- src/main/java/com/ifsp/MyHeroTraining/Security/CorsConfiguration.java
+++ src/main/java/com/ifsp/MyHeroTraining/Security/CorsConfiguration.java
@@ -8,7 +8,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("https://myherotraining.herokuapp.com/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
 }
