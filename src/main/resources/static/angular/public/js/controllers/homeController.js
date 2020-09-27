90029383b2f26fde308d3ffc19f340198ca987cb
erika-garcia <erikagarcia2@gmail.com>
2020-09-26 22:38:24 -0300

    update images

--- src/main/resources/static/angular/public/js/controllers/homeController.js
+++ src/main/resources/static/angular/public/js/controllers/homeController.js
@@ -1,11 +1,11 @@
 angular.module('myHeroTraining').controller('HomeController',function ($scope, $http, myHeroTraining,$location) {
     $scope.fotos =[
-        {url: 'https://i0.statig.com.br/bancodeimagens/54/ab/3d/54ab3dww9e6ktcnafkfdp4294.jpg'},
-        {url:'https://sportlife.com.br/wp-content/uploads/2018/12/beneficios-do-abdominal.jpg'},
-        {url:'https://uploads.metropoles.com/wp-content/uploads/2018/07/25161248/138.jpg'},
-        {url:'https://www.runtastic.com/blog/wp-content/uploads/2019/03/thumbnail_1200x800.jpg'},
-        {url:'https://www.bodynutry.ind.br/images/noticias/grd/71ba53d6fccbc4aeeb2aee3c4dc21281.jpg'},
-        {url:'https://conteudo.imguol.com.br/c/entretenimento/a7/2016/05/30/homem-fazendo-flexao-1464620034199_v2_1920x1279.jpg'},
+        {url:'https://cdn.pixabay.com/photo/2017/08/07/14/02/people-2604149_960_720.jpg'},
+        {url:'https://cdn.pixabay.com/photo/2015/07/02/10/22/training-828726_960_720.jpg'},
+        {url:'https://cdn.pixabay.com/photo/2016/03/27/07/08/man-1282232_960_720.jpg'},
+        {url:'https://cdn.pixabay.com/photo/2016/05/27/22/30/workout-1420741_960_720.jpg'},
+        {url:'https://cdn.pixabay.com/photo/2015/04/08/23/53/workout-713658_960_720.jpg'},
+        {url:'https://cdn.pixabay.com/photo/2015/07/02/10/27/training-828764_960_720.jpg'},
 
         ];
     $scope.model = {
