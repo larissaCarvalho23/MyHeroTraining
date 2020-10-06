3fa7b3c14e9646d7104b12e3e788ed2623453766
erika-garcia <erikagarcia2@gmail.com>
2020-10-06 15:03:52 -0300

    descricao-treino e update principal

--- src/main/resources/static/angular/public/js/controllers/homeController.js
+++ src/main/resources/static/angular/public/js/controllers/homeController.js
@@ -1,11 +1,12 @@
 angular.module('myHeroTraining').controller('HomeController',function ($scope, $http, myHeroTraining,$location) {
     $scope.fotos =[
-        {url: 'https://i0.statig.com.br/bancodeimagens/54/ab/3d/54ab3dww9e6ktcnafkfdp4294.jpg'},
-        {url:'https://sportlife.com.br/wp-content/uploads/2018/12/beneficios-do-abdominal.jpg'},
-        {url:'https://uploads.metropoles.com/wp-content/uploads/2018/07/25161248/138.jpg'},
-        {url:'https://www.runtastic.com/blog/wp-content/uploads/2019/03/thumbnail_1200x800.jpg'},
-        {url:'https://www.bodynutry.ind.br/images/noticias/grd/71ba53d6fccbc4aeeb2aee3c4dc21281.jpg'},
-        {url:'https://conteudo.imguol.com.br/c/entretenimento/a7/2016/05/30/homem-fazendo-flexao-1464620034199_v2_1920x1279.jpg'},
+    	{url:'angular/public/imagem/home1.jpg'},
+        {url:'angular/public/imagem/home2.jpg'},
+        {url:'angular/public/imagem/home3.jpg'},
+        {url:'angular/public/imagem/home4.jpg'},
+        {url:'angular/public/imagem/home5.jpg'},
+        {url:'angular/public/imagem/home6.jpg'},
+
 
         ];
     $scope.model = {
